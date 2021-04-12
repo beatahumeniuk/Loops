5aba8c0aaa79c8bc3f883867cdb3b2d6d1a55b2a
Beata <beatahumeniuk@gmail.com>
2021-04-12 20:53:50 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 // Napisz pętlę while, która wypisze na ekranie wszystkie liczby od 10 do 20 włącznie.
 
+// Zrób dodatkowo to samo zadanie ale korzystając z pętli do-while
+
 public class Main {
 
     public static void main(String[] args) {
